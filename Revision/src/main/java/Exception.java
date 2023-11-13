5d7bfb48dbc3380b37b2cd0906b99a7b013e2d35
Admin <Admin@DESKTOP-IEPVL3O>
2023-11-13 05:56:26 +0530

    commiting

---
+++ Revision/src/main/java/Exception.java
@@ -0,0 +1,15 @@
+import java.rmi.AccessException;
+
+public class Exception {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+int sum=100/0;
+	}
+catch(ArithmeticException e) {
+	System.out.println("errors occured");
+	System.out.println("rest of the code");
+	
+}
+}}
